e1c4e39607aecf7317d096fac17008a752eb4c3c
Esteban200589 <estebanpiccardo1989@gmail.com>
2024-04-12 18:12:07 -0300

    ajuste de mensajes de errores de Axios

--- src/views/utils/AlertModal.jsx
+++ src/views/utils/AlertModal.jsx
@@ -4,15 +4,29 @@ import { Alert } from 'react-native';
 class AlertModal {
     
     showAlert = (titile,text) => {
-        Alert.alert(
-            titile, text,
-            [
-                // { text: 'Cancelar', style: 'cancel' },
-                { text: 'Aceptar' }
-                // { text: 'Aceptar', onPress: () => console.log('Aceptar presionado') }
-            ],
-            // { cancelable: false }
-        );
+
+		if (typeof(text) === 'string') {
+			Alert.alert(
+				titile, text,
+				[
+					// { text: 'Cancelar', style: 'cancel' },
+					{ text: 'Aceptar' }
+					// { text: 'Aceptar', onPress: () => console.log('Aceptar presionado') }
+				],
+				// { cancelable: false }
+			);
+		} else {
+			var newSTR = text._j;
+			Alert.alert(
+				'', JSON.stringify(newSTR),
+				[
+					// { text: 'Cancelar', style: 'cancel' },
+					{ text: 'Aceptar' }
+					// { text: 'Aceptar', onPress: () => console.log('Aceptar presionado') }
+				],
+				// { cancelable: false }
+			);
+		}
     };
 
     showConfirmationAlert = (text) => {
